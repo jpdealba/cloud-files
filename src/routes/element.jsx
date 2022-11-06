83baaaabcb5f3b8c65a147c047b002f5a237873a
jpdealba <yoda9@live.com.mx>
2022-11-06 17:57:05 -0600

    new dowload button

--- src/routes/element.jsx
+++ src/routes/element.jsx
@@ -17,10 +17,9 @@ export default function Element({ file, index }) {
     elem.click();        
     document.body.removeChild(elem);
   }
-
   return (
    <li className="pb-3 sm:pb-4 w-full ">
-      <div className="flex items-center space-x-4 hover:cursor-pointer border  mx-3 px-5 py-2 rounded-md" onClick={() => downloadData()}>
+      <div className="flex items-center space-x-4 hover:cursor-pointer border  mx-3 px-5 py-2 rounded-md" >
          <div className="flex-shrink-0">
             <img className="w-8 h-8 rounded-full" src={file.preview_url} alt="" />
          </div>
@@ -39,7 +38,10 @@ export default function Element({ file, index }) {
             <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                {new Date(file.date).toLocaleTimeString()}
             </p>
-         </div>
+           </div>
+           <button class="hover:bg-gray-400 text-gray-800 font-bold rounded inline-flex items-center p-1" onClick={() => downloadData()}>
+            <svg class="fill-current w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+         </button>
       </div>
    </li>
   )
